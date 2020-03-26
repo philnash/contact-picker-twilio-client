424ed204becf56ca2d2e57409dc02855971f121b
Phil Nash <philnash@gmail.com>
2020-03-27 10:36:40 +1100

    Adds contact picker API

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ const Device = Twilio.Device;
 const init = async () => {
   const phoneNumInput = document.getElementById("phone-number");
   const dialBtn = document.getElementById("dial");
+  const mainElt = document.getElementsByTagName("main")[0];
   const token = await fetch("/token", { method: "POST" }).then(res =>
     res.json()
   );
@@ -26,6 +27,34 @@ const init = async () => {
       }
     });
   });
+
+  if ("contacts" in navigator && "ContactsManager" in window) {
+    const contactsBtn = document.createElement("button");
+    contactsBtn.innerText = "Choose contact";
+    mainElt.appendChild(contactsBtn);
+
+    contactsBtn.addEventListener("click", async () => {
+      try {
+        const contactProperties = ["name", "tel"];
+        const options = { multiple: false };
+        const contacts = await navigator.contacts.select(
+          contactProperties,
+          options
+        );
+        if (contacts.length > 0) {
+          const contact = contacts[0];
+          const contactNumber = contact.tel[0];
+          const contactName = contact.name[0];
+          if (contactNumber) {
+            phoneNumInput.value = contactNumber.replace(/\s/g, "");
+            dialBtn.innerText = `Dial ${contactName}`;
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
 };
 
 window.addEventListener("DOMContentLoaded", init);
