a8b30f0fc84a6670a086d2e9d0d345ec3d07abbf
Phil Nash <philnash@gmail.com>
2020-03-31 15:43:49 +1100

    Quick test of the phone number with error message

--- client/app.js
+++ client/app.js
@@ -1,9 +1,14 @@
 const Device = Twilio.Device;
+const simplePhoneNumberRegexp = /^\+[1-9]\d{1,14}$/;
 
 const init = async () => {
   const phoneNumInput = document.getElementById("phone-number");
   const dialBtn = document.getElementById("dial");
-  const mainElt = document.getElementsByTagName("main")[0];
+  const errorMessage = document.createElement("p");
+  errorMessage.classList.add("error");
+  errorMessage.innerText =
+    "Please enter the full phone number including  international code.";
+
   const token = await fetch("/token", { method: "POST" }).then(res =>
     res.json()
   );
@@ -12,14 +17,19 @@ const init = async () => {
     let connection = null;
 
     dialBtn.addEventListener("click", () => {
+      errorMessage.remove();
       if (connection === null) {
         const phoneNumber = phoneNumInput.value;
-        connection = device.connect({ To: phoneNumber });
-        connection.on("disconnect", () => {
-          connection = null;
-          dialBtn.innerText = "Dial";
-        });
-        dialBtn.innerText = "Hang up";
+        if (simplePhoneNumberRegexp.test(phoneNumber)) {
+          connection = device.connect({ To: phoneNumber });
+          connection.on("disconnect", () => {
+            connection = null;
+            dialBtn.innerText = "Dial";
+          });
+          dialBtn.innerText = "Hang up";
+        } else {
+          phoneNumInput.insertAdjacentElement("afterend", errorMessage);
+        }
       } else {
         connection.disconnect();
         connection = null;
@@ -27,34 +37,6 @@ const init = async () => {
       }
     });
   });
-
-  if ("contacts" in navigator && "ContactsManager" in window) {
-    const contactsBtn = document.createElement("button");
-    contactsBtn.innerText = "Choose contact";
-    mainElt.appendChild(contactsBtn);
-
-    contactsBtn.addEventListener("click", async () => {
-      try {
-        const contactProperties = ["name", "tel"];
-        const options = { multiple: false };
-        const contacts = await navigator.contacts.select(
-          contactProperties,
-          options
-        );
-        if (contacts.length > 0) {
-          const contact = contacts[0];
-          const contactNumber = contact.tel[0];
-          const contactName = contact.name[0];
-          if (contactNumber) {
-            phoneNumInput.value = contactNumber.replace(/\s/g, "");
-            dialBtn.innerText = `Dial ${contactName}`;
-          }
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  }
 };
 
 window.addEventListener("DOMContentLoaded", init);
